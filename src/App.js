8359b0875c42d33af1bcd9f367c7a7337599f2f7
stephengbaker <stephengbaker@yahoo.com>
2019-05-21 12:32:08 -0500

    Working code, working hot-loader, pre jasmine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component} from "react";
+import {hot} from "react-hot-loader";
 import "./App.css";
+
+function formatName(user) {
+	return user.firstName + " " + user.lastName;
+}
+
+const user = {
+	firstName: "Charlie",
+	lastName: "Smith"
+};
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <h1> Hello {formatName(user)}! </h1>
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
