facb8db309ee313564189b707ffd9d941bceb059
Stephen Baker <stephengbaker@yahoo.com>
2019-06-10 18:22:25 -0500

    Add SeeWhosHomeSpec.js
    Add first Jasmine spec file. One spec, works.

---
+++ spec/SeeWhosHomeSpec.js
@@ -0,0 +1,19 @@
+describe('App', function() {
+	it('exists', function() {
+		expect(document.getElementsByClassName('App')).toBeDefined()
+	});
+});
+
+
+
+
+
+/*describe('List items', function() {
+	it('can be added', function() {
+		document.getElementbyID('addListItemButton').trigger('click');
+		expect()
+	});
+	it('can be deleted', function() {
+
+	});
+});*/
