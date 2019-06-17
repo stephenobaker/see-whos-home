138b787313745c6f8d9da21bf64f0578fbb31b94
Stephen Baker <stephengbaker@yahoo.com>
2019-06-17 17:43:05 -0500

    Create tabbed container and convert Firebase auth functions to a React component
    CSS and Bootstrap implemented to create a container with tab headers. React components added to handle UX pass-thru on Markets Managed container. Sign in and sign out, as well as Firebase authStateChanged listening implemented inside of react component.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ var firebase = require('firebase/app');
 require('firebase/auth');
 require('firebase/database');
 
+let marketsYouManage = false;
 
 //For development only. This won't be needed when app is served via Firebase Hosting as custom URL's will be faster
 firebase.initializeApp({
@@ -22,6 +23,8 @@ firebase.initializeApp({
 
 
 var provider = new firebase.auth.GoogleAuthProvider();
+
+var database = firebase.database();
 
 
 function signIn() {
@@ -52,70 +55,212 @@ function signOut() {
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-    document.querySelector('#signOut').classList.remove('d-none');
-    document.querySelector('#signIn').classList.add('d-none')
-    document.querySelector('#userWelcome').innerHTML = 'Hi, ' + user.displayName + '!';
-    document.querySelector('#userWelcome').classList.add('d-sm-block');
-    document.querySelector('#userPicture').setAttribute('src', user.photoURL);
-    document.querySelector('#userPicture').classList.remove('d-none');
-    document.querySelector('#tempWaitMsg').classList.add('d-none');
-
+  //   document.querySelector('#signOut').classList.remove('d-none');
+  //   document.querySelector('#signIn').classList.add('d-none')
+  //   document.querySelector('#userWelcome').innerHTML = 'Hi, ' + user.displayName + '!';
+  //   document.querySelector('#userWelcome').classList.add('d-sm-block');
+  //   document.querySelector('#userPicture').setAttribute('src', user.photoURL);
+  //   document.querySelector('#userPicture').classList.remove('d-none');
+  //   document.querySelector('#tempWaitMsg').classList.add('d-none');
+  console.log('user signed in');
 
 
 
   } else {
     // No user is signed in.
-    document.querySelector('#signIn').classList.remove('d-none');
-    document.querySelector('#signOut').classList.add('d-none')
-    document.querySelector('#userWelcome').classList.remove('d-sm-block');
-    document.querySelector('#userPicture').classList.add('d-none');
-    document.querySelector('#tempWaitMsg').classList.add('d-none');
-
-
+    // document.querySelector('#signIn').classList.remove('d-none');
+    // document.querySelector('#signOut').classList.add('d-none')
+    // document.querySelector('#userWelcome').classList.remove('d-sm-block');
+    // document.querySelector('#userPicture').classList.add('d-none');
+    // document.querySelector('#tempWaitMsg').classList.add('d-none');
+    console.log('user signed out');
   }
 });
 
 
+
+function TestFirebaseAction() {
+	return(
+		<div>firebaseD</div>
+	);
+}
+
+
+
+
+
+
+
+
+class SignInButtons extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {authUser: null};
+		this.signIn = this.signIn.bind(this);
+		this.signOut = this.signOut.bind(this);
+	}
+
+	signIn() {
+		firebase.auth().signInWithRedirect(provider).then(function(result) {
+		  var token = result.credential.accessToken;
+		  var user = result.user;
+		}).catch(function(error) {
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  var email = error.email;
+		  var credential = error.credential;
+		});
+	}
+
+	signOut() {
+		firebase.auth().signOut();
+	}
+
+	componentDidMount() {
+		firebase.auth().onAuthStateChanged(authUser => {
+			authUser
+				? this.setState({authUser})
+				: this.setState({authUser: null});
+		});
+	}
+
+	render() {
+		if (this.state.authUser) {
+			return (
+				<button className="button m-2" id="signOut" onClick={this.signOut}>Sign Out</button>
+			);
+		} else {
+			return (
+				<button className="button m-2" id="signIn" onClick={this.signIn}>Sign In with Google</button>
+			);
+		}
+		
+	}
+}
+
+
+
+
+
+
+
 function NavigationBar(props) {
 	return(
-		<nav className="nav-bar d-flex align-items-center justify-content-center justify-content-sm-between flex-column flex-sm-row">
-			<div className="logo d-flex align-items-center col-auto p-2 mr-sm-auto">Who's There?</div>
+		//<div className="container-fluid">
+		<nav className="nav-bar row d-flex align-items-center justify-content-center justify-content-sm-between flex-column flex-sm-row">
+			<div className="logo d-flex align-items-center col-auto mr-sm-auto">Who's at the Market?</div>
 			<div className="d-inline-flex align-items-center flex-column flex-sm-row">
 				<div id="tempWaitMsg" className="m-2">Signing in...</div>
 				<div className="d-none m-2" id="userWelcome"></div>
 				<img className="d-none user-picture m-2" id="userPicture" src={''}/>
-				<button className="button d-none m-2" id="signIn" onClick={signIn}>Sign In with Google</button>
-				<button className="button d-none m-2" id="signOut" onClick={signOut}>Sign Out</button>
+				<SignInButtons />
 			</div>
 		</nav>
+		//</div>
 	);
 }
 
-/*function UserWindow(props) {
-	return(
-		<div>
-			<div id="userWelcome"></div>
-			<img id="userPicture" src={''}/>
-		</div>
-	);
-}*/
 
-function PublicList(props) {
-		return <div />
 
+class RenderItems extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {hasItems: false};
+	}
+
+	componentDidMount() {
+
+		if (marketsYouManage === true) {
+			this.setState({hasItems: true});
+		} else {
+			this.setState({hasItems: false});
+		}
+	}
+
+	render() {
+		return(
+			<div className="col-12 d-flex justify-content-center">
+				{(this.state.hasItems === true) ? (
+						<em>
+							You do have items in a list!
+						</em>
+				) : (
+						<em>
+							It looks like you don't manage any markets. Would you like to create a new one?
+						</em>
+				)}
+			</div>
+		);
+	}
 }
 
-function PrivateConsole(props) {
-		return <div />
+						
 
-	
+
+class CreateNewCycle extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {firstCycle: true};
+		this.handleForward = this.handleForward.bind(this);
+		this.handleBackward = this.handleBackward.bind(this);
+	}
+
+	handleForward() {
+		this.setState({firstCycle: false});
+	}
+
+	handleBackward() {
+		this.setState({firstCycle: true});
+	}
+
+	render() {
+		return(
+				<div className="tab bottom row d-flex justify-content-center align-items-center">
+				{this.state.firstCycle ? (
+					<div>
+						<RenderItems />
+						<div className="col-12 d-flex justify-content-center">
+							<button className = "button" onClick={this.handleForward}>
+								Create a new {this.props.buttonTitle}
+							</button>
+						</div>
+					</div>
+				) : (
+					<div>
+						<div className="col-12 d-flex justify-content-center">
+							<button className = "button" onClick={this.handleBackward}>
+								Go back.
+							</button>
+						</div>
+					</div>
+				)}
+			</div>
+		);
+	}
 }
 
 
 
 
+class TabbedContainer extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 
-
+	render() {
+		return(
+			<div>
+				<div className="p-2 p-sm-4 console-container">	
+					<div className="row flex-row d-flex">
+						<div className="tab live col-6 d-flex justify-content-center align-items-center">Markets you manage</div>
+						<div className="tab dead col-6 d-flex justify-content-center align-items-center">Markets you sell at</div>
+					</div>
+					<CreateNewCycle buttonTitle='Market Space'/>
+				</div>
+			</div>
+		);
+	}
+}
 
 
 
@@ -124,8 +269,10 @@ function PrivateConsole(props) {
 
 function App() {
   return(
-    <div className="app-global">
+    <div className="container-fluid app-global">
       <NavigationBar />
+      <TabbedContainer />
+      <div id='testFirebaseTarget'/>
     </div>
   );
 }
